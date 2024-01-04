44137c4573d52b90f8561ccb9c3c25b4d7c0303c
PoceroJunior <pocerojunior@gmail.com>
2024-01-04 19:19:16 +0100

    Conexión con Websockets conseguida.
    Hay que seguir las instrucciones del txt "Instrucciones para correr servidor". Usar siempre Windows.

---
+++ server.js
@@ -0,0 +1,20 @@
+
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+
+// Configuracion para servir archivos estaticos desde la carpeta 'public'
+app.use(express.static('public'));
+
+// Ruta para servir el archivo HTML en la ruta raiz
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+// Resto de la configuracion del WebSocket...
+
+// Iniciar el servidor
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
